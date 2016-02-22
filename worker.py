ff4b72b3b16367af890de2732519b445779b05d3
Alimohammad Rabbani <amre2005@gmail.com>
2016-02-22 16:06:48 -0500

    fixes

--- worker.py
+++ worker.py
@@ -1,6 +1,6 @@
 from DataCollector import ConvertRaw
 import pigpio
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import subprocess
 
 CLK_PIN = 4
@@ -14,11 +14,17 @@ def configure_pins():
     GPIO.setup(2, GPIO.IN)
 
 if __name__ == "__main__":
+    print "pigpiod -s2"
+    subprocess.call(["pigpiod", "-s2"])
     configure_pins()
-    subprocess.call(["sudo", "pigpiod", "-s2"])
+    print "pigs no"
     subprocess.call(["pigs", "no"])
+    print "cp /dev/pigpio1 /home/pi/log&"
     subprocess.call(["cp", "/dev/pigpio0", "/home/pi/log&"])
+    print "pigs nb 0 $((1<<%d))" % CLK_PIN
     subprocess.call(["pigs", "nb", "0", "$((1<<%d))" % CLK_PIN])
+    print "sleep 30"
     subprocess.call(["sleep", "30"])
+    print "pigs nc 0"
     subprocess.call(["pigs", "nc", "0"])
     # ConvertRaw.convert("/Users/Alimohammad/Documents/Development/sonar-leak/data.bin", "data_out.bin")
