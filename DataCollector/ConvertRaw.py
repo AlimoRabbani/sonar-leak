019f9a29b7e4d58f2e84f58d86bd7fb865afc5ca
Alimohammad Rabbani <amre2005@gmail.com>
2016-02-25 15:55:00 -0500

    fix

--- DataCollector/ConvertRaw.py
+++ DataCollector/ConvertRaw.py
@@ -4,6 +4,7 @@ import struct
 CLK_PIN = 4
 CS_PIN = 18
 DATA_PIN = 2
+VOLTAGE = 3.315
 
 
 def convert(in_name, out_name):
@@ -32,6 +33,7 @@ def print_sample(sample, message=""):
     rep = (clk_bit << 2) + (cs_bit << 1) + data_bit
     print("{0:03b}".format(rep) + " " + message)
 
+
 def read_till_cs_low(input_file):
     seq = 0
     try:
@@ -48,28 +50,33 @@ def read_till_cs_low(input_file):
                 # print_sample(sample, "SKIP CS!")
                 return seq
     except Exception, e:
-        print(e.message)
-        exit(1)
+        raise
 
 
 def skip_first_eight_data_bits(input_file):
-    for i in range(0, 16):
-        input_file.read(8)
-        sample = input_file.read(4)
-        # print_sample(sample, "SKIP BITs >> %d" % i)
+    try:
+        for i in range(0, 16):
+            input_file.read(8)
+            sample = input_file.read(4)
+            # print_sample(sample, "SKIP BITs >> %d" % i)
+    except Exception, e:
+        raise
+
 
 def read_data_bits(input_file):
     data = 0
     bit_counter = 0
-    while bit_counter < 16:
-        input_file.read(8)
-        sample = input_file.read(4)
-        int_value = struct.unpack('<I', sample)[0]
-        clk_bit = int_value >> CLK_PIN & 1
-        data_bit = int_value >> DATA_PIN & 1
-        if clk_bit == 1:
-            data += (data_bit << (15 - bit_counter))
-            # print_sample(sample, "READ BIT >> %d" % (15 - bit_counter))
-            bit_counter += 1
-    return data
-
+    try:
+        while bit_counter < 16:
+            input_file.read(8)
+            sample = input_file.read(4)
+            int_value = struct.unpack('<I', sample)[0]
+            clk_bit = int_value >> CLK_PIN & 1
+            data_bit = int_value >> DATA_PIN & 1
+            if clk_bit == 1:
+                data += (data_bit << (15 - bit_counter))
+                # print_sample(sample, "READ BIT >> %d" % (15 - bit_counter))
+                bit_counter += 1
+        return data
+    except Exception, e:
+        raise
--- DataCollector/Sampler.py
+++ DataCollector/Sampler.py
@@ -10,6 +10,7 @@ def start(duration):
     print("Staring copy from %s" % source_filename)
     cp_cmd = "cp " + source_filename + " /home/pi/log&"
     os.system(cp_cmd)
-    sampling_cmd = "pigs nb " + pigpio_n + " 16;" + "sleep " + str(duration) + ";" + "pigs nc " + pigpio_n
+    sampling_cmd = "pigs nb " + pigpio_n + " 16; sleep " + str(duration) + "; pigs nc " + pigpio_n
     print("Running %s" % sampling_cmd)
     os.system(sampling_cmd)
+    print("Sampling Finished...")
