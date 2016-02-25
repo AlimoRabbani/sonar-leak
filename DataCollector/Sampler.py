df2a1941f30d607f8dd74e2d9dfa8faeb63a3e6a
Alimohammad Rabbani <amre2005@gmail.com>
2016-02-25 15:46:01 -0500

    fix

--- DataCollector/Sampler.py
+++ DataCollector/Sampler.py
@@ -10,6 +10,6 @@ def start(duration):
     print("Staring copy from %s" % source_filename)
     cp_cmd = "cp " + source_filename + " /home/pi/log&"
     os.system(cp_cmd)
-    sampling_cmd = "pigs nb " + pigpio_n + ";" + "sleep " + str(duration) + ";" + "pigs nc " + pigpio_n
+    sampling_cmd = "pigs nb " + pigpio_n + " 16;" + "sleep " + str(duration) + ";" + "pigs nc " + pigpio_n
     print("Running %s" % sampling_cmd)
     os.system(sampling_cmd)
