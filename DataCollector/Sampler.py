a8e206b5e2de3e8fe22533921180d97a52ef0902
Alimohammad Rabbani <amre2005@gmail.com>
2016-02-25 15:44:57 -0500

    cp_cmd

--- DataCollector/Sampler.py
+++ DataCollector/Sampler.py
@@ -8,7 +8,8 @@ def start(duration):
     pigpio_n = pigpio_n.strip()
     source_filename = "/dev/pigpio" + pigpio_n
     print("Staring copy from %s" % source_filename)
-    subprocess.call(["cp", source_filename, "/home/pi/log&"])
+    cp_cmd = "cp " + source_filename + " /home/pi/log&"
+    os.system(cp_cmd)
     sampling_cmd = "pigs nb " + pigpio_n + ";" + "sleep " + str(duration) + ";" + "pigs nc " + pigpio_n
     print("Running %s" % sampling_cmd)
     os.system(sampling_cmd)
