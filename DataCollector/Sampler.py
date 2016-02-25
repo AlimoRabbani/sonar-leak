980d62ccb0a86e8d893c871fa0c065cd28bc3f59
Alimohammad Rabbani <amre2005@gmail.com>
2016-02-25 15:42:56 -0500

    Improvements

---
+++ DataCollector/Sampler.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import subprocess
+import os
+
+
+def start(duration):
+    pigpio_n = subprocess.Popen(["pigs", "no"], stdout=subprocess.PIPE).communicate()[0]
+    pigpio_n = pigpio_n.strip()
+    source_filename = "/dev/pigpio" + pigpio_n
+    print("Staring copy from %s" % source_filename)
+    subprocess.call(["cp", source_filename, "/home/pi/log&"])
+    sampling_cmd = "pigs nb " + pigpio_n + ";" + "sleep " + str(duration) + ";" + "pigs nc " + pigpio_n
+    print("Running %s" % sampling_cmd)
+    os.system(sampling_cmd)
--- worker.py
+++ worker.py
@@ -1,7 +1,8 @@
 from DataCollector import ConvertRaw
+from DataCollector import Sampler
 import pigpio
 import RPi.GPIO as GPIO
-import subprocess
+import time
 
 CLK_PIN = 4
 
@@ -14,17 +15,7 @@ def configure_pins():
     GPIO.setup(2, GPIO.IN)
 
 if __name__ == "__main__":
-    print "pigpiod -s2"
-    subprocess.call(["pigpiod", "-s2"])
     configure_pins()
-    print "pigs no"
-    subprocess.call(["pigs", "no"])
-    print "cp /dev/pigpio1 /home/pi/log&"
-    subprocess.call(["cp", "/dev/pigpio1", "/home/pi/log&"])
-    print "pigs nb 0 %d" % (1 << CLK_PIN)
-    subprocess.call(["pigs", "nb", "0", "%d" % (1 << CLK_PIN)])
-    print "sleep 30"
-    subprocess.call(["sleep", "30"])
-    print "pigs nc 0"
-    subprocess.call(["pigs", "nc", "0"])
-    # ConvertRaw.convert("/Users/Alimohammad/Documents/Development/sonar-leak/data.bin", "data_out.bin")
+    Sampler.start(10)
+    time.sleep(1)
+    ConvertRaw.convert("/home/pi/log", "/home/pi/log_cleaned")
